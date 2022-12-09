83109f354523fae5cc85806b15c424a3d8ae3733
vbogdanovich13 <vbogdanovich13@gmail.com>
2022-12-09 23:29:14 +0300

    fix HelloWorldController! MathController+multiply

--- src/main/java/by/vadzim13/sandbox/HelloWorldController.java
+++ src/main/java/by/vadzim13/sandbox/HelloWorldController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWOrldController {
+public class HelloWorldController {
     @GetMapping("/hello")
     public String HelloWorld() {
-        return "Hello MINSK";
+        return "Hello Minsk, good bye Moscow!";
     }
 }
--- src/main/java/by/vadzim13/sandbox/MathController.java
+++ src/main/java/by/vadzim13/sandbox/MathController.java
@@ -1,14 +1,27 @@
 package by.vadzim13.sandbox;
 
-import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/math")
 public class MathController {
-    @GetMapping("/math/add/{a:\\d+}/{b:\\d+}")
-    public Integer Add (@PathParam(value = "a") Integer a, @PathParam(value = "b") Integer b) {
-       return a + b;
+    @GetMapping("add/{a}/{b}")
+    public Integer add(
+            @PathVariable("a") Integer a,
+            @PathVariable("b") Integer b
+    ) {
+        return a + b;
+    }
+
+    @GetMapping("multiply/{c}/{d}")
+    public Integer multiply(
+            @PathVariable("c") Integer c,
+            @PathVariable("d") Integer d
+    ) {
+        return c * d;
     }
 
 }
